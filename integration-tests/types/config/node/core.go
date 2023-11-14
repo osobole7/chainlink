0924699e783a525ee58d03e08371f3e501e7a7a3
patrickhuie19 <patrick.huie@smartcontract.com>
2023-11-14 18:01:42 -0500

    feature/tracing-credentials: adding Tracing.Mode to configure insecure credentials explicitly

--- integration-tests/types/config/node/core.go
+++ integration-tests/types/config/node/core.go
@@ -134,11 +134,12 @@ func WithTracing() NodeConfigOpt {
 			Enabled:         utils2.Ptr(true),
 			CollectorTarget: utils2.Ptr("otel-collector:4317"),
 			// ksortable unique id
-			NodeID: utils2.Ptr(ksuid.New().String()),
+			NodeID:        utils2.Ptr(ksuid.New().String()),
+			SamplingRatio: utils2.Ptr(1.0),
+			Mode:          utils2.Ptr("insecure"),
 			Attributes: map[string]string{
 				"env": "smoke",
 			},
-			SamplingRatio: utils2.Ptr(1.0),
 		}
 	}
 }
