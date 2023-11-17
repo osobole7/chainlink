fc034d516c402189208578f33f82e8ef377e8bf4
Fergal Gribben <fergal.gribben@smartcontract.com>
2023-11-17 19:24:47 +0000

    WIP implement a keepers transmitter

--- core/services/relay/evm/ocr2keeper.go
+++ core/services/relay/evm/ocr2keeper.go
@@ -19,10 +19,15 @@ import (
 
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 
+	txmgrcommon "github.com/smartcontractkit/chainlink/v2/common/txmgr"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm"
+	txm "github.com/smartcontractkit/chainlink/v2/core/chains/evm/txmgr"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services/job"
+	"github.com/smartcontractkit/chainlink/v2/core/services/keystore"
+	"github.com/smartcontractkit/chainlink/v2/core/services/ocrcommon"
 	"github.com/smartcontractkit/chainlink/v2/core/services/pipeline"
+	functionsRelay "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/functions"
 	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/types"
 )
 
@@ -45,7 +50,7 @@ type OCR2KeeperProvider interface {
 
 // OCR2KeeperRelayer contains the relayer and instantiating functions for OCR2Keeper providers.
 type OCR2KeeperRelayer interface {
-	NewOCR2KeeperProvider(rargs relaytypes.RelayArgs, pargs relaytypes.PluginArgs) (OCR2KeeperProvider, error)
+	NewOCR2KeeperProvider(rargs relaytypes.RelayArgs, pargs relaytypes.PluginArgs, eth keystore.Eth) (OCR2KeeperProvider, error)
 }
 
 // ocr2keeperRelayer is the relayer with added DKG and OCR2Keeper provider functions.
@@ -68,20 +73,30 @@ func NewOCR2KeeperRelayer(db *sqlx.DB, chain evm.Chain, pr pipeline.Runner, spec
 	}
 }
 
-func (r *ocr2keeperRelayer) NewOCR2KeeperProvider(rargs relaytypes.RelayArgs, pargs relaytypes.PluginArgs) (OCR2KeeperProvider, error) {
-	cfgWatcher, err := newOCR2KeeperConfigProvider(r.lggr, r.chain, rargs)
+func (r *ocr2keeperRelayer) NewOCR2KeeperProvider(rargs relaytypes.RelayArgs, pargs relaytypes.PluginArgs, ethKeystore keystore.Eth) (OCR2KeeperProvider, error) {
+	configWatcher, err := newOCR2KeeperConfigProvider(r.lggr, r.chain, rargs)
 	if err != nil {
 		return nil, err
 	}
 
-	gasLimit := cfgWatcher.chain.Config().EVM().OCR2().Automation().GasLimit()
-	contractTransmitter, err := newPipelineContractTransmitter(r.lggr, rargs, pargs.TransmitterID, &gasLimit, cfgWatcher, r.spec, r.pr)
+	//gasLimit := configWatcher.chain.Config().EVM().OCR2().Automation().GasLimit()
+	//contractTransmitter, err := newPipelineContractTransmitter(r.lggr, rargs, pargs.TransmitterID, &gasLimit, cfgWatcher, r.spec, r.pr)
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	//var pluginConfig ocr2keeper.PluginConfig
+	//if err2 := json.Unmarshal(pargs.PluginConfig, &pluginConfig); err2 != nil {
+	//	return nil, err2
+	//}
+
+	contractTransmitter, err := newKeepersContractTransmitter(1, rargs, pargs.TransmitterID, configWatcher, ethKeystore, r.lggr)
 	if err != nil {
 		return nil, err
 	}
 
 	return &ocr2keeperProvider{
-		configWatcher:       cfgWatcher,
+		configWatcher:       configWatcher,
 		contractTransmitter: contractTransmitter,
 	}, nil
 }
@@ -172,4 +187,75 @@ func newOCR2KeeperConfigProvider(lggr logger.Logger, chain evm.Chain, rargs rela
 		relayConfig.FromBlock,
 		rargs.New,
 	), nil
+}
+
+func newKeepersContractTransmitter(contractVersion uint32, rargs relaytypes.RelayArgs, transmitterID string, configWatcher *configWatcher, ethKeystore keystore.Eth, lggr logger.Logger) (ContractTransmitter, error) {
+	var relayConfig types.RelayConfig
+	if err := json.Unmarshal(rargs.RelayConfig, &relayConfig); err != nil {
+		return nil, err
+	}
+	var fromAddresses []common.Address
+	sendingKeys := relayConfig.SendingKeys
+	if !relayConfig.EffectiveTransmitterID.Valid {
+		return nil, errors.New("EffectiveTransmitterID must be specified")
+	}
+	effectiveTransmitterAddress := common.HexToAddress(relayConfig.EffectiveTransmitterID.String)
+
+	sendingKeysLength := len(sendingKeys)
+	if sendingKeysLength == 0 {
+		return nil, errors.New("no sending keys provided")
+	}
+
+	// If we are using multiple sending keys, then a forwarder is needed to rotate transmissions.
+	// Ensure that this forwarder is not set to a local sending key, and ensure our sending keys are enabled.
+	for _, s := range sendingKeys {
+		if sendingKeysLength > 1 && s == effectiveTransmitterAddress.String() {
+			return nil, errors.New("the transmitter is a local sending key with transaction forwarding enabled")
+		}
+		if err := ethKeystore.CheckEnabled(common.HexToAddress(s), configWatcher.chain.Config().EVM().ChainID()); err != nil {
+			return nil, errors.Wrap(err, "one of the sending keys given is not enabled")
+		}
+		fromAddresses = append(fromAddresses, common.HexToAddress(s))
+	}
+
+	scoped := configWatcher.chain.Config()
+	strategy := txmgrcommon.NewQueueingTxStrategy(rargs.ExternalJobID, scoped.OCR2().DefaultTransactionQueueDepth(), scoped.Database().DefaultQueryTimeout())
+
+	var checker txm.TransmitCheckerSpec
+	if configWatcher.chain.Config().OCR2().SimulateTransactions() {
+		checker.CheckerType = txm.TransmitCheckerTypeSimulate
+	}
+
+	gasLimit := configWatcher.chain.Config().EVM().GasEstimator().LimitDefault()
+	ocr2Limit := configWatcher.chain.Config().EVM().GasEstimator().LimitJobType().OCR2()
+	if ocr2Limit != nil {
+		gasLimit = *ocr2Limit
+	}
+
+	transmitter, err := ocrcommon.NewTransmitter(
+		configWatcher.chain.TxManager(),
+		fromAddresses,
+		gasLimit,
+		effectiveTransmitterAddress,
+		strategy,
+		checker,
+		configWatcher.chain.ID(),
+		ethKeystore,
+	)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create transmitter")
+	}
+
+	keepersTransmitter, err := functionsRelay.NewFunctionsContractTransmitter(
+		configWatcher.chain.Client(),
+		configWatcher.contractABI,
+		transmitter,
+		configWatcher.chain.LogPoller(),
+		lggr,
+		nil,
+		contractVersion,
+	)
+
+	return keepersTransmitter, err
 }
